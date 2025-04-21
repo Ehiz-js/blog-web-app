f74839ce922b2083a6fce58999fb502d36164a72
Ehiz-js <azamegbesamuel@gmail.com>
2025-04-21 09:14:32 +0100

    js

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,5 +1,17 @@
+//
 let heroSection = document.querySelector(".hero");
 let scrollPosition = window.scrollY;
+let float = document.querySelector(".form");
+let addBtn = document.querySelector(".addBtn");
+let closeBtn = document.querySelector(".closeBtn");
+
 window.addEventListener("scroll", () => {
 	heroSection.classList.toggle("blurred", window.scrollY > 50);
+});
+
+addBtn.addEventListener("click", () => {
+	float.style.display = "flex";
+});
+closeBtn.addEventListener("click", () => {
+	float.style.display = "none";
 });
